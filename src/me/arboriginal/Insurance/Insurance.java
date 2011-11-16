15434dbea54a8c823d48e6c2a8f60fbb5725f017
arboriginal <webastien+arboriginal@gmail.com>
2011-11-16 21:36:08 +0100

    Add submaterials support (such as colored wools), items condition and others minor changes

--- src/me/arboriginal/Insurance/Insurance.java
+++ src/me/arboriginal/Insurance/Insurance.java
@@ -21,7 +21,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Insurance extends JavaPlugin {
@@ -34,11 +33,8 @@ public class Insurance extends JavaPlugin {
   @Override
   public void onEnable() {
     initConfig();
-
-    PluginManager pm = getServer().getPluginManager();
-    InsuranceEntityListener el = new InsuranceEntityListener(this);
-
-    pm.registerEvent(Event.Type.ENTITY_DEATH, el, Priority.Normal, this);
+    getServer().getPluginManager().registerEvent(Event.Type.ENTITY_DEATH,
+        new InsuranceEntityListener(this), Priority.Normal, this);
   }
 
   @Override
@@ -81,7 +77,24 @@ public class Insurance extends JavaPlugin {
         amount += price;
 
         if (log) {
-          prime += stack.getAmount() + "x " + stack.getType() + ": " + price + "\n";
+          Material type = stack.getType();
+          List<String> subMaterials = getSubMaterials(type);
+          float condition = calculateCondition(stack);
+          String details = "";
+
+          if (subMaterials.size() > 0) {
+            details += subMaterials.get(stack.getData().getData()) + " ";
+          }
+
+          if (condition < 1) {
+            details += "condition: " + (100 * condition) + "%";
+          }
+
+          if (!details.equals("")) {
+            details = " (" + details.trim() + ") ";
+          }
+
+          prime += stack.getAmount() + "x " + type + details + ": " + price + "\n";
         }
       }
     }
@@ -104,7 +117,8 @@ public class Insurance extends JavaPlugin {
     FileHandler fh = null;
 
     try {
-      fh = new FileHandler(getDataFolder() + "/primesLog.txt", 10000, 1, true);
+      fh = new FileHandler(getDataFolder() + "/primesLog.txt",
+          config.getInt("Insurance.log_filesize"), 1, true);
       fh.setFormatter(new InsuranceLogFormatter());
 
       logger.addHandler(fh);
@@ -140,7 +154,7 @@ public class Insurance extends JavaPlugin {
     return amount;
   }
 
-  public float calculatePrice(ItemStack stack) {
+  private float calculatePrice(ItemStack stack) {
     float price = getItemPrice(stack);
 
     if (price > 0) {
@@ -150,48 +164,274 @@ public class Insurance extends JavaPlugin {
     return price;
   }
 
-  public float getItemPrice(ItemStack stack) {
-    float price = 0;
+  private float getItemPrice(ItemStack stack) {
+    float finalPrice = 0;
 
     if (stack != null) {
       String key = "Insurance.primes." + stack.getType();
 
       if (config.contains(key)) {
-        price += config.getDouble(key);
+        Object price = config.get(key);
+
+        if (!(price instanceof Double)) {
+          key += "." + getSubMaterials(stack.getType()).get(stack.getData().getData());
+          price = config.getDouble(key);
+        }
+
+        finalPrice += (Double) price * calculateCondition(stack);
       }
     }
 
-    return price;
+    return finalPrice;
   }
 
   public void payPlayer(Player player, float amount) {
-    player.sendMessage(config.getString("Insurance.pay_message")
-        .replace("<player>", player.getName()).replace("<amount>", "" + amount));
+    player.sendMessage(renderString("pay_message", player.getName(), amount));
 
-    getServer().dispatchCommand(
-        getServer().getConsoleSender(),
-        config.getString("Insurance.pay_command").replace("<player>", player.getName())
-            .replace("<amount>", "" + amount));
+    getServer().dispatchCommand(getServer().getConsoleSender(),
+        renderString("pay_command", player.getName(), amount));
   }
 
   private void initConfig() {
     config = getConfig();
     boolean edited = checkConfigValue("Insurance.log_prime", true);
 
+    edited = checkConfigValue("Insurance.log_filesize", 10000) || edited;
+    edited = checkConfigValue("Insurance.calc_message",
+        "<player>, if you die now, you will receive <amount> dollars.") || edited;
     edited = checkConfigValue("Insurance.pay_message",
         "<player>, you will receive soon <amount> dollars for your lost stuff.") || edited;
     edited = checkConfigValue("Insurance.pay_command",
-        "tell <player> Your admin didn't made his job, you loose <amount> dollars :D") || edited;
+        "tell <player> Your admin didn't make his job, you loose <amount> dollars :D") || edited;
+    edited = checkConfigValue("Insurance.consider_condition", true) || edited;
 
     Material[] values = Material.values();
 
     for (int i = 0; i < values.length; i++) {
-      edited = checkConfigValue("Insurance.primes." + values[i], 0F) || edited;
+      Double defaultValue = 0.0;
+      List<String> subMaterials = getSubMaterials(values[i]);
+
+      if (subMaterials.size() == 0) {
+        edited = checkConfigValue("Insurance.primes." + values[i], defaultValue) || edited;
+      }
+      else {
+        if (config.contains("Insurance.primes." + values[i])) {
+          Object value = config.get("Insurance.primes." + values[i]);
+
+          if (value instanceof Double) {
+            defaultValue += (Double) value;
+          }
+        }
+
+        for (String sm : subMaterials) {
+          edited = checkConfigValue("Insurance.primes." + values[i] + "." + sm, defaultValue)
+              || edited;
+        }
+      }
     }
 
     if (edited) {
       saveConfig();
       System.out.println("[Insurance] Omitted values has been added to your config file.");
+    }
+  }
+
+  private List<String> getSubMaterials(Material material) {
+    List<String> values = new ArrayList<String>();
+
+    switch (material) {
+      case SAPLING:
+      case LOG:
+      case LEAVES:
+        values.add(0, "Usual");
+        values.add(1, "Spruce");
+        values.add(2, "Birch");
+        break;
+
+      case HUGE_MUSHROOM_1:
+      case HUGE_MUSHROOM_2:
+        values.add(0, "Fleshy");
+        values.add(1, "Top_north_west");
+        values.add(2, "Top_north");
+        values.add(3, "Top_north_east");
+        values.add(4, "Top_west");
+        values.add(5, "Top");
+        values.add(6, "Top_east");
+        values.add(7, "Top_south_west");
+        values.add(8, "Top_south");
+        values.add(9, "Top_south_east");
+        values.add(10, "Stem");
+        break;
+
+      case COAL:
+        values.add(0, "Coal");
+        values.add(1, "Charcoal");
+        break;
+
+      case INK_SACK:
+        values.add(0, "Ink_Sac");
+        values.add(1, "Rose_Red");
+        values.add(2, "Cactus_Green");
+        values.add(3, "Cocoa_Beans");
+        values.add(4, "Lapis_Lazuli");
+        values.add(5, "Purple_Dye");
+        values.add(6, "Cyan_Dye");
+        values.add(7, "Light_Gray_Dye");
+        values.add(8, "Gray_Dye");
+        values.add(9, "Pink_Dye");
+        values.add(10, "Lime_Dye");
+        values.add(11, "Dandelion_Yellow");
+        values.add(12, "Light_Blue_Dye");
+        values.add(13, "Magenta_Dye");
+        values.add(14, "Orange_Dye");
+        values.add(15, "Bone_Meal");
+        break;
+
+      case WOOL:
+        values.add(0, "White");
+        values.add(1, "Orange");
+        values.add(2, "Magenta");
+        values.add(3, "Light_Blue");
+        values.add(4, "Yellow");
+        values.add(5, "Lime");
+        values.add(6, "Pink");
+        values.add(7, "Gray");
+        values.add(8, "Light_Gray");
+        values.add(9, "Cyan");
+        values.add(10, "Purple");
+        values.add(11, "Blue");
+        values.add(12, "Brown");
+        values.add(13, "Green");
+        values.add(14, "Red");
+        values.add(15, "Black");
+        break;
+
+      case STEP:
+      case DOUBLE_STEP:
+        values.add(0, "Stone_Slab");
+        values.add(1, "Sandstone_Slab");
+        values.add(2, "Wooden_Slab");
+        values.add(3, "Cobblestone_Slab");
+        values.add(4, "Brick_Slab");
+        values.add(5, "Stone_Brick_Slab");
+        values.add(6, "Stone_Slab");
+        break;
+
+      case SMOOTH_BRICK:
+        values.add(0, "Normal");
+        values.add(1, "Mossy");
+        values.add(2, "Cracked");
+        break;
+    }
+
+    return values;
+  }
+
+  private float calculateCondition(ItemStack stack) {
+    if (!config.getBoolean("Insurance.consider_condition")) {
+      return 100;
+    }
+
+    int maxDurability = getMaxDurability(stack.getType());
+
+    if (maxDurability == 0) {
+      return 100;
+    }
+
+    return 1 - stack.getDurability() / (float) maxDurability;
+  }
+
+  private int getMaxDurability(Material material) {
+    switch (material) {
+      case FLINT_AND_STEEL:
+      case FISHING_ROD:
+        return 65;
+
+      case SHEARS:
+        return 239;
+
+      case GOLD_SWORD:
+      case GOLD_PICKAXE:
+      case GOLD_SPADE:
+      case GOLD_AXE:
+      case GOLD_HOE:
+        return 33;
+
+      case WOOD_SWORD:
+      case WOOD_PICKAXE:
+      case WOOD_SPADE:
+      case WOOD_AXE:
+      case WOOD_HOE:
+        return 60;
+
+      case STONE_SWORD:
+      case STONE_PICKAXE:
+      case STONE_SPADE:
+      case STONE_AXE:
+      case STONE_HOE:
+        return 132;
+
+      case IRON_SWORD:
+      case IRON_PICKAXE:
+      case IRON_SPADE:
+      case IRON_AXE:
+      case IRON_HOE:
+        return 251;
+
+      case DIAMOND_SWORD:
+      case DIAMOND_PICKAXE:
+      case DIAMOND_SPADE:
+      case DIAMOND_AXE:
+      case DIAMOND_HOE:
+        return 1562;
+
+      case LEATHER_HELMET:
+        return 34;
+      case LEATHER_CHESTPLATE:
+        return 49;
+      case LEATHER_LEGGINGS:
+        return 46;
+      case LEATHER_BOOTS:
+        return 40;
+
+      case CHAINMAIL_HELMET:
+        return 67;
+      case CHAINMAIL_CHESTPLATE:
+        return 96;
+      case CHAINMAIL_LEGGINGS:
+        return 92;
+      case CHAINMAIL_BOOTS:
+        return 79;
+
+      case GOLD_HELMET:
+        return 68;
+      case GOLD_CHESTPLATE:
+        return 96;
+      case GOLD_LEGGINGS:
+        return 92;
+      case GOLD_BOOTS:
+        return 80;
+
+      case IRON_HELMET:
+        return 136;
+      case IRON_CHESTPLATE:
+        return 192;
+      case IRON_LEGGINGS:
+        return 184;
+      case IRON_BOOTS:
+        return 160;
+
+      case DIAMOND_HELMET:
+        return 272;
+      case DIAMOND_CHESTPLATE:
+        return 384;
+      case DIAMOND_LEGGINGS:
+        return 368;
+      case DIAMOND_BOOTS:
+        return 320;
+
+      default:
+        return 0;
     }
   }
 
@@ -231,7 +471,8 @@ public class Insurance extends JavaPlugin {
       return false;
     }
 
-    sender.sendMessage(ChatColor.GREEN + "Insurance prime: " + calculatePrime(player));
+    sender.sendMessage(ChatColor.GREEN
+        + renderString("calc_message", player.getName(), calculatePrime(player)));
 
     return true;
   }
@@ -251,6 +492,11 @@ public class Insurance extends JavaPlugin {
     }
 
     return false;
+  }
+
+  private String renderString(String key, String player, float amount) {
+    return config.getString("Insurance." + key).replace("<player>", player)
+        .replace("<amount>", "" + amount);
   }
 
   // Internal class
